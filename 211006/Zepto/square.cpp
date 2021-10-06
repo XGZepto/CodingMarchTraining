032cd7984d965d567c35300fa628b35b9d762efc
Zepto <xgzepto@gmail.com>
2021-10-06 17:27:21 +0800

    Square Permutation

---
+++ 211006/Zepto/square.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+unordered_map<string, int> cnt;
+long long ans, n;
+
+int main(){
+    scanf("%lld", &n);
+    for (register int i = 1; 1ll * i * i <= n; ++i){
+        long long temp = 1ll * i * i;
+        string buff = to_string(temp);
+        sort(buff.begin(), buff.end());
+        ans += cnt[buff]++;
+    }
+    printf("%lld", ans);
+}
